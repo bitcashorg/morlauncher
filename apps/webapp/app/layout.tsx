3876abf84d5dd9b491e0c65ccd2fd82fc4f5b8bb
Leandro Gavidia Santamaria <93232139+leandrogavidia@users.noreply.github.com>
2024-07-11 14:51:04 -0400

    [webapp] feat: multibase config + google analytics (#3)
    * config: add multibase init config
    
    * config: add google analytics component
    
    * chore: remove custom google analytics component
    
    * feat: create multibase hook
    
    * chore: remove unnecesary file
    
    * config: update multibase hook and google id
    
    * config: add google analytics env variable and update multibase hook
    
    * chore: improve the type in the multibase hook and update env variables configuration for multibase and google analytics

--- apps/webapp/app/layout.tsx
+++ apps/webapp/app/layout.tsx
@@ -41,7 +41,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
           </Providers>
         </GlobalStoreProvider>
 
-        <GoogleAnalytics gaId="G-78N0Z7NPQJ" />
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_APP_GOOGLE_ID || ""} />
         <SpeedInsights />
       </body>
     </html>
