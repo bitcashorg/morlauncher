f9bc0700958d6a2e96851671cc65383fb6349174
Leandro Gavidia Santamaria <93232139+leandrogavidia@users.noreply.github.com>
2024-08-28 17:15:08 -0400

    config: add multibase provider (#4)

--- apps/webapp/hooks/use-multibase.ts
+++ apps/webapp/hooks/use-multibase.ts
@@ -1,26 +1,19 @@
-import { init, identify, track } from '@multibase/js'
-
-const MULTIBASE_API_KEY = process.env.MULTIBASE_API_KEY || ""
-
-if (!MULTIBASE_API_KEY) {
-    throw new Error("Missing MULTIBASE_API_KEY")
-}
+import { identify, track } from '@multibase/js'
 
 export function useMultibase() {
-    init(MULTIBASE_API_KEY)
+  const identifyUser = (
+    address: `0x${string}`,
+    properties: Record<string, any>,
+  ) => identify(address, properties)
 
-    const identifyUser = (properties: Record<string, any>, address = '0xd8da6bf26964af9d7eed9e03e53415d37aa96045') =>
-        identify({
-            address,
-            properties
-        })
+  const trackInteraction = (
+    label: string,
+    properties: MultibaseTrackPropertiesProps,
+  ) => track(label, properties)
 
-    const trackInteraction = (label: string, properties: MultibaseTrackPropertiesProps) =>
-        track(label, properties)
-
-    return { trackInteraction, identifyUser }
+  return { trackInteraction, identifyUser }
 }
 
 interface MultibaseTrackPropertiesProps {
-    [key: string]: any;
+  [key: string]: any
 }
--- apps/webapp/lib/config.ts
+++ apps/webapp/lib/config.ts
@@ -5,6 +5,9 @@ const env = getValidSmartsaleEnv(process.env.NEXT_PUBLIC_APP_ENV || 'test')
 export const appConfig = {
   env,
   eosRpc: 'https://eos.greymass.com',
+  multibase: {
+    key: process.env.NEXT_PUBLIC_MULTIBASE_API_KEY || '',
+  },
   features: {
     enableWalletAccess: process.env.NEXT_PUBLIC_ENABLE_WALLET_REDIRECT === 'true'
   },
